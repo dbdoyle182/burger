59eefeaf7c3b7c7899f208ffd63e681a9cb6a66d
Daniel Doyle <dbdoyle182@gmail.com>
2018-04-03 19:37:48 -0400

    {

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,19 @@
+// Dependencies
 var express = require('express');
 
 var router = express.Router();
 
 var burger = require('../models/burger.js');
+
+router.get('/', function(req, res) {
+    burger.all((data) => {
+        var hbsObject = {
+            burgers: data 
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+module.exports = router
 
--- server.js
+++ server.js
@@ -2,3 +2,30 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Set up static content from the public directory
+app.use(express.static('public'));
+
+// Set up bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set up Handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import router
+var routes = require('./controllers/burgers_controller.js')
+
+app.use(routes);
+
+// Start up the server and notify server-side that it is running
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+});
