4bfcf3400489e2306bf35eded6c5e3d74f898256
Daniel Doyle <dbdoyle182@gmail.com>
2018-04-03 18:10:04 -0400

    select all orm function working

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,21 @@
-var connection = require('./connection.js');
+var connection = require('../config/connection.js');
 
 var orm = {
-    selectAll: () => {},
+    selectAll: (tableInput, cb) => {
+        var queryString = 'SELECT * FROM ' + tableInput + ';';
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
     insertOne: () => {},
     updateOne: () => {}
 }
+
+orm.selectAll('burgers', (result) => {
+    console.log(result);
+});
 
 module.exports = orm;
