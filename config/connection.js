34bba9b6bc230104f7aca0d2906e222af6858e34
Daniel Doyle <dbdoyle182@gmail.com>
2018-04-04 20:50:31 -0400

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'taffy182!',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.envJAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'taffy182!',
+        database: 'burgers_db'
+    });
+};
 
 connection.connect((err) => {
     if (err) {
